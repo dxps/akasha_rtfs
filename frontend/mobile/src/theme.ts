ef5cf7f9440e0e8bfb5d5d8ff4044b75838e99d5
dxps <marius.ileana@gmail.com>
2026-04-16 18:11:37 +0300

    cometics: light theme bg color upd

--- frontend/mobile/src/theme.ts
+++ frontend/mobile/src/theme.ts
@@ -3,7 +3,7 @@ export type ThemeMode = 'light' | 'dark'
 export const themes = {
 	light: {
 		accent: '#af3a03',
-		background: '#d1d2d1',
+    background: '#e2e0d7',
 		blue: '#076678',
 		border: '#d5c4a1',
 		foreground: '#504945',
